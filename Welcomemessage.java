7e42470c933bd32a72383af7f15f20bd4ec8164d
Parvsub <Praveenboda421@gmail.com>
2023-02-09 10:45:53 +0530

    Added: Welcome meassage printed

---
+++ Welcomemessage.java
@@ -0,0 +1,7 @@
+package com.employeeWageusingoops;
+
+public class Welcomemessage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to EmployeeWage computation Problem");
+    }
+}
